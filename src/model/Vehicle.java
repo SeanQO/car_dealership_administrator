35e3b4c1a0c377c5c44a580f18ce55965a291fb5
anderson-154 <54720103+anderson-154@users.noreply.github.com>
2020-11-25 18:13:04 -0500

    add atributes vehicle class test

--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -2,4 +2,48 @@ package model;
 
 public class Vehicle {
 
+	private double totalSalePrice;
+	private double basePrice;
+	private String brand;
+	private double displacement;
+	private int mileage;
+	private boolean used;
+	
+	public Vehicle(double totalSalePrice, double basePrice, String brand, double displacement, int mileage,
+			boolean used) {
+		super();
+		this.totalSalePrice = totalSalePrice;
+		this.basePrice = basePrice;
+		this.brand = brand;
+		this.displacement = displacement;
+		this.mileage = mileage;
+		this.used = used;
+	}
+
+	public double getTotalSalePrice() {
+		return totalSalePrice;
+	}
+
+	public double getBasePrice() {
+		return basePrice;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public double getDisplacement() {
+		return displacement;
+	}
+
+	public int getMileage() {
+		return mileage;
+	}
+
+	public boolean isUsed() {
+		return used;
+	}
+	
+	
+	
 }
