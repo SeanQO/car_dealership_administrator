5a2222de863e7230c70de2fc6d28b4f782ff0994
anderson-154 <54720103+anderson-154@users.noreply.github.com>
2020-11-28 18:24:15 -0500

    class test

--- test/model/CompanyTest.java
+++ test/model/CompanyTest.java
@@ -4,25 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-import customException.EmptyDataException;
-
 class CompanyTest {
 
-	private Company company;
-	private Admin admin;
-	
-	public void setup1() {
-		company = new Company("salesCars", 1231, admin);
-	}
-	
-	public void setup2() {
-		company = new Company("salesCars", 1231, admin);
-		company.	}
-	
 	@Test
 	void test() {
-		setup2();
-		assertEquals(1,company.getDealers().size());
+		fail("Not yet implemented");
 	}
 
 }
