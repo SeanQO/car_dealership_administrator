257ce1376dc4d274e0f3be53a3c2ab125e21fa64
SeanQO <seanquintero3@gmail.com>
2020-11-28 21:44:34 -0500

    Added dealer button functionalities, added methods and atributes to the gui

--- src/ui/DealershipGUI.java
+++ src/ui/DealershipGUI.java
@@ -17,6 +17,12 @@ public class DealershipGUI {
 	
 	private Stage dealerStage;
 	
+	private Stage listStage;
+	
+	private Stage registerStage;
+	
+	// main window attributes
+	
     @FXML
     private AnchorPane mainPane;
 
@@ -37,16 +43,33 @@ public class DealershipGUI {
 
     @FXML
     private TableColumn<?, ?> columnCredits;
-
+    
+    //dealer window attributes 
     @FXML
     private Label mainNitLabel;
 
     @FXML
     private Label mainAdressLabel;
     
+    @FXML
+    private Label dealerAdminNameLabel;
+
+    @FXML
+    private Label dealerTotalSalesLabel;
+
+    @FXML
+    private Label dealerTotalClientsLabel;
+
+    @FXML
+    private Label dealerTotalEarningsLabel;
+
+
+    
     public DealershipGUI() {
 		// TODO Auto-generated constructor stub
 	}
+    
+    // main window action
     
     @FXML
     void openDealer(ActionEvent event) throws IOException{
@@ -61,6 +84,47 @@ public class DealershipGUI {
 		dealerStage = stage;
 		stage.show();
 		
+    }
+    
+    // dealer window actions
+    @FXML
+    void addVehicle(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void clientList(ActionEvent event) throws IOException{
+    	
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/lists/Client_list.fxml"));
+    	fxmlLoader.setController(this);
+    	Parent registerPane = fxmlLoader.load();
+    	
+    	Scene scene = new Scene(registerPane);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setTitle("Update client");
+		registerStage = stage;
+		stage.show();
+    }
+
+    @FXML
+    void registerClient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerSeller(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellerList(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vehicleList(ActionEvent event) {
+
     }
 
 }
