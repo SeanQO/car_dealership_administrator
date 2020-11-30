f198cd92ab08908cd82aedf87268a88344e85989
SeanQO <seanquintero3@gmail.com>
2020-11-29 20:16:27 -0500

    Added register client and seller funcionality tu UI, and added limit to one registration window, added alert to close opened registrarion window and open new one

--- src/ui/DealershipGUI.java
+++ src/ui/DealershipGUI.java
@@ -1,28 +1,29 @@
 package ui;
 
 import java.io.IOException;
+import java.util.Optional;
+
+import com.sun.xml.internal.ws.wsdl.writer.document.OpenAtts;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class DealershipGUI {
 	
-	private Stage dealerStage;
-	
-	private Stage listStage;
-	
-	private Stage registerStage;
-	
-	// main window attributes
-	
+	// *************************** main window attributes ***************************
     @FXML
     private AnchorPane mainPane;
 
@@ -44,7 +45,7 @@ public class DealershipGUI {
     @FXML
     private TableColumn<?, ?> columnCredits;
     
-    //dealer window attributes 
+    // *************************** dealer window attributes ***************************
     @FXML
     private Label mainNitLabel;
 
@@ -63,11 +64,66 @@ public class DealershipGUI {
     @FXML
     private Label dealerTotalEarningsLabel;
     
+    // *************************** register client window attributes ***************************
+    // *rC* register client window indicator
+    @FXML
+    private TextField rCTxtName;
+
+    @FXML
+    private TextField rCTxtLastname;
+
+    @FXML
+    private TextField rCTxtId;
+
+    @FXML
+    private TextField rCTxtEmail;
+
+    @FXML
+    private TextField rCTxtPhoneNumber;
+
+    @FXML
+    private ComboBox<?> rCsellerComboBox;
+    
+    // *************************** register client window attributes ***************************
+    // *rS* register client window indicator
+    @FXML
+    private TextField rStxtName;
+
+    @FXML
+    private TextField rStxtLastname;
+
+    @FXML
+    private TextField rStxtId;
+
+    @FXML
+    private TextField rStxtEmail;
+
+    @FXML
+    private TextField rStxtPhoneNumber;
+
+    @FXML
+    private TextField rStxtSalary;
+
+    @FXML
+    private ComboBox<?> rSadminComboBox;
+
+    // *************************** GUI attributes and builder***************************
+    private Stage dealerStage;
+	
+	private Stage listStage;
+	
+	private Stage registerStage;
+	
+	private Stage alertStage;
+    
     public DealershipGUI() {
-		// TODO Auto-generated constructor stub
+		dealerStage = null;
+		listStage = null;
+		registerStage = null;
+		alertStage = null;
 	}
     
-    // main window action
+    // *************************** main window action ***************************
     
     @FXML
     void openDealer(ActionEvent event) throws IOException{
@@ -78,53 +134,154 @@ public class DealershipGUI {
     	Scene scene = new Scene(dealerPane);
 		Stage stage = new Stage();
 		stage.setScene(scene);
-		stage.setTitle("Update client");
+		stage.setTitle("DealerShip");
 		dealerStage = stage;
 		stage.show();
 		
     }
     
-    // dealer window actions
+    // *************************** dealer window actions ***************************
+    
+    // *************************** clients menu
+    
     @FXML
-    void addVehicle(ActionEvent event) {
+    void openRegisterClient(ActionEvent event) throws IOException{
+    	if (registerStage == null) {
+    		openRegisterClient();
+    		
+		}else {
+			boolean runRegister = false;
+			
+			runRegister = multipleRegisterAlert("Register client");
+					
+			if (runRegister) {
+				openRegisterClient();
+				
+			}
+			
+			
+		} 	
     	
     }
-
-    @FXML
-    void clientList(ActionEvent event) throws IOException{
+    
+    private void openRegisterClient() throws IOException{
+    	System.out.println("open register client working");
     	
-    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/lists/Client_list.fxml"));
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/register/register_clients.fxml"));
     	fxmlLoader.setController(this);
-    	Parent registerPane = fxmlLoader.load();
+    	Parent registerClientPane = fxmlLoader.load();
     	
-    	Scene scene = new Scene(registerPane);
+    	Scene scene = new Scene(registerClientPane);
 		Stage stage = new Stage();
 		stage.setScene(scene);
-		stage.setTitle("Update client");
+		stage.setTitle("Register client");
+		registerStage = stage;
+		stage.show();
+    }
+    
+    @FXML
+    void openClientList(ActionEvent event) throws IOException{
+    	System.out.println("open client list working");
+    }
+    
+    // *************************** sellers menu
+    @FXML
+    void openRegisterSeller(ActionEvent event) throws IOException{
+    	if (registerStage == null) {
+    		openRegisterSeller();
+    		
+		}else {
+			boolean runRegister = false;
+			
+			runRegister = multipleRegisterAlert("Register seller");
+					
+			if (runRegister) {
+				openRegisterSeller();
+				
+			}
+			
+			
+		}
+		
+    }
+    
+    private void openRegisterSeller() throws IOException{
+    	System.out.println("open reister seller working");
+
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/register/register_sellers.fxml"));
+    	fxmlLoader.setController(this);
+    	Parent registerSellerPane = fxmlLoader.load();
+    	
+    	Scene scene = new Scene(registerSellerPane);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setTitle("Register client");
 		registerStage = stage;
 		stage.show();
     }
 
     @FXML
+    void openSellerList(ActionEvent event) {
+    	System.out.println("open seller list working");
+    }
+    
+    // *************************** vehicles menu
+    @FXML
+    void openAddVehicle(ActionEvent event) {
+    	System.out.println("open add vehicle working");
+    }
+    
+    @FXML
+    void openVehicleList(ActionEvent event) {
+    	System.out.println("open vehicle list working");
+    }
+
+    // *************************** register client window actions ***************************
+   
+    @FXML
     void registerClient(ActionEvent event) {
+    	System.out.println("Register client working");
+    	registerStage.close();
+    	registerStage = null;
 
     }
+    
+ // *************************** register seller window actions ***************************
 
     @FXML
     void registerSeller(ActionEvent event) {
-
+    	System.out.println("Register seller working");
+    	registerStage.close();
+    	registerStage = null;
     }
-
-    @FXML
-    void sellerList(ActionEvent event) {
-
+    
+    // *************************** alerts ***************************
+     
+    private boolean multipleRegisterAlert(String title) {
+    	
+    	Alert multiAlert = new Alert(AlertType.CONFIRMATION);
+    	
+		multiAlert.setTitle("Multiple registrations");
+		multiAlert.setHeaderText( registerStage.getTitle() + " window open. \n One registration window allowed.");
+		
+		ButtonType closeButton = new ButtonType("Close this message");
+		ButtonType closeAndOpenButton = new ButtonType("Close " + registerStage.getTitle() + " window, and open " + title + " window" );
+		multiAlert.getButtonTypes().setAll(closeButton, closeAndOpenButton);
+		
+		Optional<ButtonType> result = multiAlert.showAndWait();
+		
+		if (result.get() == closeButton) {
+			
+		}else if (result.get() == closeAndOpenButton) {
+			registerStage.close();
+			registerStage = null;
+			return true;
+			
+		}
+		
+		return false;
+		
     }
-
-    @FXML
-    void vehicleList(ActionEvent event) {
-
-    }
-
 }
 
     
