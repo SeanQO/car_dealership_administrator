b3f1112e1e9ab51e0783140b918aa112d98dce01
SeanQO <seanquintero3@gmail.com>
2020-12-02 19:05:48 -0500

    Exit program confirmation added

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,18 @@
 package ui;
 
+import java.util.Optional;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application{
 	private DealershipGUI dealershipGUI;
@@ -27,9 +35,31 @@ public class Main extends Application{
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Company");
+		
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			
+            @Override
+            public void handle(WindowEvent e) {
+            	Alert alert = new Alert(AlertType.CONFIRMATION);
+        		
+        		alert.setTitle("Exit confirmation");
+        		alert.setHeaderText("");
+        		alert.setContentText("Are you sure you want to exit the program?");
+        		
+        		Optional<ButtonType> result = alert.showAndWait();
+        		
+        		if (result.get() == ButtonType.OK) {
+        			Platform.exit();
+        	        System.exit(0);
+        		}else {
+        			e.consume();
+        		}
+            	
+            }
+        });
+		
 		primaryStage.show();
 		
 	}
-	
 	
 }
